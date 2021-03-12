89a46fb32624d52bbc837c331272c3dfbb335583
Queena SupremeBliss <queena@Queenas-MacBook-Pro.local>
2021-03-12 11:19:44 -0600

    OK page

---
+++ UnobtrusiveJavaScript/ok.js
@@ -0,0 +1,12 @@
+
+
+//document.getElementById("ok").onclick = okClick; // null 
+//function okClick() { alert(" You clicked ok!! ") }
+// called when page loads; sets up event handlers
+function pageLoad() {
+    document.getElementById("ok").onclick = okClick;
+}
+function okClick() {
+    alert(" You clicked ok!!");
+}
+window.onload = pageLoad;  // onload is a browser event
