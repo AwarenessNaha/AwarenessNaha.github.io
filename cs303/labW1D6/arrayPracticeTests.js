18e2ce7605a4ac9408ff1d0dbc6799ef3f12c27c
Queena SupremeBliss <queena@Queenas-MacBook-Pro.local>
2021-03-28 15:41:32 -0500

    Comment out

--- cs303/labW1D6/arrayPracticeTests.js
+++ cs303/labW1D6/arrayPracticeTests.js
@@ -1,6 +1,6 @@
 "use strict";
 
-/* comment out the node specific code when going to the browser*/
+/* comment out the node specific code when going to the browser
 const assert = require("assert");  //always need this with node
 const myExports = require("./arrayPractice.js");  //with node need the name of your file with your functions here
 const doubleNums = myExports.doubleNums;  //do this for all of the functions used in the Mocha tests
@@ -14,7 +14,7 @@ const avgOfNumbers = myExports.avgOfNumbers;
 const maxOfNumbers = myExports.maxOfNumbers;
 const avgOfEvenAge = myExports.avgOfEvenAge;
 const avgOfOddAge = myExports.vgOfOddAge;
-
+*/
 
 let numArray;
 let peopleArray;
