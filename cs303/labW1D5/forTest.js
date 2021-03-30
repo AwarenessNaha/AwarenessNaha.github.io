c531e547b01518360a00e315d68c5c51d2a7c17a
Queena SupremeBliss <queena@Queenas-MacBook-Pro.local>
2021-03-30 18:23:38 -0500

    W2D2HW

--- cs303/labW1D5/forTest.js
+++ cs303/labW1D5/forTest.js
@@ -1,28 +1,9 @@
-function filterRangeInPlace(arr, a, b) {
+let gaia = 5;
 
-    for (let i = 0; i < arr.length; i++) {
-        let val = arr[i];
-
-        // remove if outside of the interval
-        if (val < a || val > b) {
-            arr.splice(i, 1);
-            i--;
-        }
-    }
-
+function bueatyGaia(gaia) {
+    gaia = gaia * gaia;
+    return gaia;
 }
 
-let arr = [5, 6, 3, 8, 1, 9];
-
-filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
-
-console.log(arr); // [3, 1]
-
-function sortedArray(arr) {
-    let arrCopy = arr.slice();
-    let sortedArr = arrCopy.sort();
-    return sortedArr;
-}
-let arr5 = ["a", "n", "m", "b", "t", "c"];
-console.log(sortedArray(arr5));
-console.log(arr5);
+console.log(bueatyGaia(5));
+console.log(gaia);
